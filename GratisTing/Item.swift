2d0dbdedc64eb075a6c18046b3a78380b77b8245
Thomas Attermann <thomas_5009@hotmail.com>
2016-05-12 14:07:08 +0200

    Add Latitude & Longitude attributes to Item class

--- GratisTing/Item.swift
+++ GratisTing/Item.swift
@@ -16,27 +16,33 @@ class Item {
     var imageURL: String = ""
     var createdAt: NSDate = NSDate()
     var owner: User?
+    var latitude: Double = 0
+    var longitude: Double = 0
     
     /*
      Constructor without id parameter. Used when creating Item object in the app. Id will be assigned in DB.
      */
-    init (title: String, description: String, imageURL: String, createdAt: NSDate, owner: User) {
+    init (title: String, description: String, imageURL: String, createdAt: NSDate, owner: User, latitude: Double, longitude: Double) {
         self.title = title
         self.description = description
         self.imageURL = imageURL
         self.createdAt = createdAt
         self.owner = owner
+        self.latitude = latitude
+        self.longitude = longitude
     }
     
     /**
      Constructor with id attribute. Used when parsing JSON to Item object.
      */
-    init (id: String, title: String, description: String, imageURL: String, createdAt: NSDate, owner: User) {
+    init (id: String, title: String, description: String, imageURL: String, createdAt: NSDate, owner: User, latitude: Double, longitude: Double) {
         self.title = title
         self.description = description
         self.imageURL = imageURL
         self.createdAt = createdAt
         self.owner = owner
+        self.latitude = latitude
+        self.longitude = longitude
     }
     
 }
