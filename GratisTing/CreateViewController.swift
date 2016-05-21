af581cd3a0e4257fb2d093bb7e846b0b881a5f9c
steffen25 <acbolic@gmail.com>
2016-05-21 21:19:11 +0200

    Add image picker to create item

--- GratisTing/CreateViewController.swift
+++ GratisTing/CreateViewController.swift
@@ -7,24 +7,32 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class CreateViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+class CreateViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     // MARK: - Dependencies
     let dao = AppDelegate.dao
     let auth = AppDelegate.authentication
+    let imagePicker = UIImagePickerController()
     
     // NYT
     //let auth = AppDelegate.authentication
 
-
     // MARK: - Components
+    @IBOutlet weak var itemImageView: UIImageView!
     @IBOutlet weak var titleTextfield: UITextField!
     @IBOutlet weak var descriptionTextfield: UITextField!
     @IBOutlet weak var categoryTextField: UITextField!
     var categoryPicker: UIPickerView!
     
     // MARK: - Actions
+    @IBAction func selectImagePressed(sender: AnyObject) {
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
     @IBAction func createItemButton(sender: AnyObject) {
         
         let title = titleTextfield.text!
@@ -70,7 +78,9 @@ class CreateViewController: UIViewController, UIPickerViewDelegate, UIPickerView
     // MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Setup image picker
+        imagePicker.delegate = self
+        itemImageView.hidden = true
         // Setup category picker
         categoryPicker = UIPickerView()
         categoryPicker.delegate = self
@@ -106,6 +116,29 @@ class CreateViewController: UIViewController, UIPickerViewDelegate, UIPickerView
     
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
         return false
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            itemImageView.image = pickedImage
+            itemImageView.hidden = false
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func didMediaFromSource(sourceType: UIImagePickerControllerSourceType) {
+        
+        let mediaTypes = UIImagePickerController.availableMediaTypesForSourceType(sourceType)
+        imagePicker.mediaTypes = mediaTypes!
+        imagePicker.sourceType = sourceType
+        imagePicker.allowsEditing = true
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
     }
     
 }
